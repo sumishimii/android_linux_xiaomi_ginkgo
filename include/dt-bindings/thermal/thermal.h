8c812cc404505a127f0b32a0eac5e5a4fa3af9cb
sumishimii <88553244+sumishimii@users.noreply.github.com>
2025-10-27 18:06:06 +0530

    thermal

--- include/dt-bindings/thermal/thermal.h
+++ include/dt-bindings/thermal/thermal.h
@@ -13,6 +13,9 @@
 /* On cooling devices upper and lower limits */
 #define THERMAL_NO_LIMIT		(~0)
 #define THERMAL_MAX_LIMIT		(THERMAL_NO_LIMIT - 1)
+#define THERMAL_LIMIT_MINUS_1  (THERMAL_MAX_LIMIT - 1)
+#define THERMAL_LIMIT_MINUS_5  (THERMAL_MAX_LIMIT - 5)
+#define THERMAL_LIMIT_MINUS_6  (THERMAL_MAX_LIMIT - 6)
 
 #endif
 
