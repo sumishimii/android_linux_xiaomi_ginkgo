0f89b69c9f23bf494249ae5ece3df90d20e0d9cd
sumishimii <88553244+sumishimii@users.noreply.github.com>
2025-10-27 12:51:01 +0530

    Update mqueue.c

--- ipc/mqueue.c
+++ ipc/mqueue.c
@@ -1506,18 +1506,18 @@ COMPAT_SYSCALL_DEFINE5(mq_timedsend, mqd_t, mqdes,
 	return do_mq_timedsend(mqdes, u_msg_ptr, msg_len, msg_prio, p);
 }
 
-COMPAT_SYSCALL_DEFINE5(mq_timedreceive, mqd_t, mqdes,
-		       char __user *, u_msg_ptr,
-		       compat_size_t, msg_len, unsigned int __user *, u_msg_prio,
-		       const struct compat_timespec __user *, u_abs_timeout)
-{
-	struct timespec64 ts, *p = NULL;
-	if (u_abs_timeout) {
-		int res = compat_prepare_timeout(u_abs_timeout, &ts);
-		if (res)
-			return res;
-		p = &ts;
-	}
+//COMPAT_SYSCALL_DEFINE5(mq_timedreceive, mqd_t, mqdes,
+//		       char __user *, u_msg_ptr,
+//		       compat_size_t, msg_len, unsigned int __user *, u_msg_prio,
+//		       const struct compat_timespec __user *, u_abs_timeout)
+//{
+//	struct timespec64 ts, *p = NULL;
+//	if (u_abs_timeout) {
+//		int res = compat_prepare_timeout(u_abs_timeout, &ts);
+//		if (res)
+//			return res;
+//		p = &ts;
+//	}
 	return do_mq_timedreceive(mqdes, u_msg_ptr, msg_len, u_msg_prio, p);
 }
 
