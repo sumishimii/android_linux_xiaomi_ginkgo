d43767320ca224dfaa15fafe08d4f6c755688480
sumishimii <88553244+sumishimii@users.noreply.github.com>
2025-10-28 09:27:41 +0530

    Update read_write_compat.c

--- fs/read_write_compat.c
+++ fs/read_write_compat.c
@@ -14,16 +14,16 @@ asmlinkage ssize_t compat_sys_preadv(compat_ulong_t fd,
                                      const struct compat_iovec __user *vec,
                                      compat_ulong_t vlen, u32 pos_low, u32 pos_high)
 {
-    loff_t pos = ((loff_t)pos_high << 32) | pos_low;
-    return sys_preadv(fd, (const struct iovec __user *)vec, vlen, pos);
+    return sys_preadv(fd, (const struct iovec __user *)vec, vlen,
+                      pos_low, pos_high);
 }
 
 asmlinkage ssize_t compat_sys_pwritev(compat_ulong_t fd,
                                       const struct compat_iovec __user *vec,
                                       compat_ulong_t vlen, u32 pos_low, u32 pos_high)
 {
-    loff_t pos = ((loff_t)pos_high << 32) | pos_low;
-    return sys_pwritev(fd, (const struct iovec __user *)vec, vlen, pos);
+    return sys_pwritev(fd, (const struct iovec __user *)vec, vlen,
+                       pos_low, pos_high);
 }
 
 #ifdef CONFIG_PREADV2
@@ -31,15 +31,15 @@ asmlinkage ssize_t compat_sys_preadv2(compat_ulong_t fd,
                                       const struct compat_iovec __user *vec,
                                       compat_ulong_t vlen, u32 pos_low, u32 pos_high, rwf_t flags)
 {
-    loff_t pos = ((loff_t)pos_high << 32) | pos_low;
-    return sys_preadv2(fd, (const struct iovec __user *)vec, vlen, pos, flags);
+    return sys_preadv2(fd, (const struct iovec __user *)vec, vlen,
+                       pos_low, pos_high, flags);
 }
 
 asmlinkage ssize_t compat_sys_pwritev2(compat_ulong_t fd,
                                        const struct compat_iovec __user *vec,
                                        compat_ulong_t vlen, u32 pos_low, u32 pos_high, rwf_t flags)
 {
-    loff_t pos = ((loff_t)pos_high << 32) | pos_low;
-    return sys_pwritev2(fd, (const struct iovec __user *)vec, vlen, pos, flags);
+    return sys_pwritev2(fd, (const struct iovec __user *)vec, vlen,
+                        pos_low, pos_high, flags);
 }
 #endif
